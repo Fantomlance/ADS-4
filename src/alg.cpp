5ca16497f4e448abcbeb1030bc6e74230d19cf9a
Fantomlance <125268565+Fantomlance@users.noreply.github.com>
2023-05-14 23:11:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,58 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+int countPairs2(int* arr, int len, int value) {
+    int count = 0;
+    for (int i = len - 1; i >= 0 ; i--) {
+        if (arr[i] <= value) {
+            for (int j = i + 1; j < len; j++) {
+                if (arr[i] + arr[j] == value) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  int cur = 0;
+  int mid = 0;
+  for (int i = 0; i < len - 1; i++) {
+    int left = i;
+    int right = len;
+    while (left < right - 1) {
+      mid = (left + right) / 2;
+      if (arr[i] + arr[mid] == value) {
+        count++;
+        cur = mid + 1;
+        while (arr[i] + arr[cur] == value && cur < right) {
+          count++;
+          cur++;
+        }
+        cur = mid - 1;
+        while (arr[i] + arr[cur] == value && cur > left) {
+          count++;
+          cur--;
+        }
+        break;
+      }
+      if (arr[i] + arr[mid] > value) {
+        right = mid;
+      } else {
+        left = mid;
+      }
+    }
+  }
+  return count;
 }
